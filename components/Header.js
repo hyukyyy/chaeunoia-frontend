d2bffe415b9b25d41746827c6b788109f45b1637
HyukYoon <56344824+hyukyyy@users.noreply.github.com>
2021-12-03 18:25:48 +0900

    헤더 링크 태그 적용 및 스타일 조정

--- components/Header.js
+++ components/Header.js
@@ -1,33 +1,42 @@
+import Link from "next/link";
 import styles from "../styles/Header.module.scss";
+
 const Header = () => {
   const mainMenus = [
     {
       text: "menu1",
+      url: "",
     },
     {
       text: "menu2",
+      url: "",
     },
     {
       text: "menu3",
+      url: "",
     },
     {
       text: "menu4",
+      url: "",
     },
     {
       text: "menu5",
+      url: "",
     },
   ];
 
   const infoMenus = [
     {
       text: "sign in",
+      url: "",
     },
     {
       text: "log in",
+      url: "",
     },
-
     {
       text: "my menu",
+      url: "",
     },
   ];
   return (
@@ -35,12 +44,24 @@ const Header = () => {
       <div className={styles.title}>Chaeunoia</div>
       <div className={styles.mainMenuWrapper}>
         {mainMenus.map((menu) => {
-          return <div className={styles.menuBox}>{menu.text}</div>;
+          return (
+            <div className={styles.menuBox}>
+              <Link href={menu.url}>
+                <a className={styles.linkText}>{menu.text}</a>
+              </Link>
+            </div>
+          );
         })}
       </div>
       <div className={styles.infoMenuWrapper}>
         {infoMenus.map((menu) => {
-          return <div className={styles.menuBox}>{menu.text}</div>;
+          return (
+            <div className={styles.menuBox}>
+              <Link href={menu.url}>
+                <a className={styles.linkText}>{menu.text}</a>
+              </Link>
+            </div>
+          );
         })}
       </div>
     </div>
